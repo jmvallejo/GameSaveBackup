7c6b3f123dd9662b7815cfeebd97290a354eb51f
Juan Manuel Vallejo <jmvallejo@gmail.com>
2018-07-31 23:25:48 -0500

    Add basic electron dependency and config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Test')
+import { app, Menu, Tray } from 'electron'
+import path from 'path'
+
+let tray = null
+app.once('ready', () => {
+  const iconPath = path.join(__dirname, '..', 'img', 'iconTemplate.png')
+  tray = new Tray(iconPath)
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  tray.setToolTip('Game Save Backup')
+  tray.setContextMenu(contextMenu)
+})
